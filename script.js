7ad3c1737e8903b31fb3461d3c2aa8d7597bb544
Ahadullahkhan1 <163107353+Ahadullahkhan1@users.noreply.github.com>
2024-12-07 10:16:40 +0400

    Complete Portoflio

--- script.js
+++ script.js
@@ -4,41 +4,78 @@ let wordIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
 const typeEffect = () => {
-    const currentWord = words[wordIndex];
-    const currentChar = currentWord.substring(0, charIndex);
-    dynamicText.textContent = currentChar;
-    dynamicText.classList.add("stop-blinking");
-    if (!isDeleting && charIndex < currentWord.length) {
-        charIndex++;
-        setTimeout(typeEffect, 200);
-    } else if (isDeleting && charIndex > 0) {
-        charIndex--;
-        setTimeout(typeEffect, 100);
-    } else {
-        isDeleting = !isDeleting;
-        dynamicText.classList.remove("stop-blinking");
-        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
-        setTimeout(typeEffect, 1200);
-    }
+  const currentWord = words[wordIndex];
+  const currentChar = currentWord.substring(0, charIndex);
+  dynamicText.textContent = currentChar;
+  dynamicText.classList.add("stop-blinking");
+  if (!isDeleting && charIndex < currentWord.length) {
+    charIndex++;
+    setTimeout(typeEffect, 200);
+  } else if (isDeleting && charIndex > 0) {
+    charIndex--;
+    setTimeout(typeEffect, 100);
+  } else {
+    isDeleting = !isDeleting;
+    dynamicText.classList.remove("stop-blinking");
+    wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+    setTimeout(typeEffect, 1200);
+  }
 }
 typeEffect();
 var tabButtons = document.querySelectorAll(".tabContainer .buttonContainer button");
 var tabPanels = document.querySelectorAll(".tabContainer  .tabPanel");
 
 function showPanel(panelIndex, colorCode) {
-    tabButtons.forEach(function (node) {
-        node.style.backgroundColor = "";
-        node.style.color = "";
-    });
-    tabButtons[panelIndex].style.backgroundColor = colorCode;
-    tabButtons[panelIndex].style.backgroundColor = "#DD0505";
-    tabButtons[panelIndex].style.color = "#fff";
-    tabPanels.forEach(function (node) {
-        node.style.display = "none";
-    });
-    tabPanels[panelIndex].style.display = "block";
-    tabPanels[panelIndex].style.backgroundColor = colorCode;
+  tabButtons.forEach(function (node) {
+    node.style.backgroundColor = "";
+    node.style.color = "";
+  });
+  tabButtons[panelIndex].style.backgroundColor = colorCode;
+  tabButtons[panelIndex].style.backgroundColor = "#DD0505";
+  tabButtons[panelIndex].style.color = "#fff";
+  tabPanels.forEach(function (node) {
+    node.style.display = "none";
+  });
+  tabPanels[panelIndex].style.display = "block";
+  tabPanels[panelIndex].style.backgroundColor = colorCode;
 }
 showPanel(0, '');
 
+const form = document.getElementById('form');
+const result = document.getElementById('result');
 
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
+
+  fetch('https://api.web3forms.com/submit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: json
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        result.innerHTML = "Form submitted successfully";
+      } else {
+        console.log(response);
+        result.innerHTML = json.message;
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      result.innerHTML = "Something went wrong!";
+    })
+    .then(function () {
+      form.reset();
+      setTimeout(() => {
+        result.style.display = "none";
+      }, 3000);
+    });
+});
